ab4dfc14c0c4dfde9da8ef5432db4df732a4c31b
Jurph <j.r.parsons@gmail.com>
2023-12-02 11:23:16 -0500

    Cleaned up, used data class, better all over.

--- day02/day02p2.py
+++ day02/day02p2.py
@@ -1,52 +1,31 @@
 # Advent of Code 2023
-# Day 2, Part 1
+# Day 2, Part 2
 # The elf's cube-choosing game 
 
 from dataclasses import dataclass
 
-class Game():
-    def __init__(self, gamestring):
-        gameindex = 0
-        red = 0
-        blu = 0
-        grn = 0
-        index, data = gamestring.split(':')
-        gameindex = int(index[4:])
-        draws = data.split(';')
-        # print("Game {}: {} draws of cubes.".format(gameindex, len(draws)))
-        for d in draws:
-            draw = d.split(',')
-            for cubes in draw:
-                r = 0
-                g = 0
-                b = 0
-                if " red" in cubes:
-                    r = int(cubes.strip(" red"))
-                elif " green" in cubes:
-                    g = int(cubes.strip(" green"))
-                elif " blue" in cubes:
-                    b = int(cubes.strip(" blue"))
-                else:
-                    print("GLITCH: seems like you didn't draw any cubes?")
-                    pass
-                red = max(r, red)
-                grn = max(g, grn)
-                blu = max(b, blu)
-        self.index = gameindex
-        self.red = red
-        self.grn = grn
-        self.blu = blu
-        self.isPossible = self.couldhappen()
-        # A game's "power", per the problem statement for Part 2, is the 
-        # product of the highest number of red, green, and blue cubes seen
-        self.power = int(red * grn * blu)
-        return 
+@dataclass(frozen=True)
+class Game(object):
+    index: int
+    maxRed: int
+    maxGreen: int
+    maxBlue: int
+    
+    def couldHappen(self) -> bool:
+        return (self.maxRed <= 12) and (self.maxGreen <= 13) and (self.maxBlue <= 14)
 
-    def couldhappen(self):
-        # A game is "possible", per the problem statement for Part 1, if we've
-        # never seen more than 12 red, 13 green, or 14 blue cubes 
-        if (self.red <= 12) and (self.grn <= 13) and (self.blu <= 14):
-            return True
+    def power(self) -> int:
+        return self.maxRed * self.maxGreen * self.maxBlue 
+    
+def MakeGame(gamestring: str) -> Game:
+    prefix, data = gamestring.strip().split(':')
+    _, index = prefix.split()
+    counts = {'red': 0, 'green': 0, 'blue': 0}
+    for draw in data.split(';'):
+        for count in draw.split(','):
+            qty, color = count.split()
+            counts[color] = max(counts[color], int(qty))
+    return Game(index=int(index), maxRed=counts['red'], maxGreen=counts['green'], maxBlue=counts['blue'])
 
 # Problem() is my class that ingests the day's input and structures it for easy computation
 class Problem():
@@ -54,18 +33,32 @@ class Problem():
         games = [] 
         lines = open(filename).read().splitlines()
         for line in lines:
-            games.append(Game(line))
+            games.append(MakeGame(line))
         self.games = games
 
+# Sums the powers of games in a given problem 
+def getPowerTotal(problem):
+    subtotal = 0
+    for game in problem.games:
+        subtotal += game.power()
+    return subtotal
+
+# Sums the indices of valid games in a given problem 
+def getValidGames(problem):
+    subtotal = 0
+    for game in problem.games:
+        if game.couldHappen():
+            subtotal += game.index
+    return subtotal
+    
 def main():
     # Ingest and format the data
     t = Problem("C:\\Users\\Jurph\\Documents\\Python Scripts\\aoc2023\\day02\\test.txt")
     p = Problem("C:\\Users\\Jurph\\Documents\\Python Scripts\\aoc2023\\day02\\input.txt")
 
-    subtotal = 0
-    for game in p.games:
-        subtotal += game.power
-    print("Total power: {}".format(subtotal))
+    # Run the solvers against each problem 
+    print("TEST CASE: valid games = {}, total power = {}".format(getValidGames(t), getPowerTotal(t)))
+    print("PROD CASE: valid games = {}, total power = {}".format(getValidGames(p), getPowerTotal(p)))
 
 if __name__ == "__main__":
     main()
