841f18b9c22a8ad8a98bf3fb959226a72e233cf4
Jurph <j.r.parsons@gmail.com>
2023-12-01 19:46:05 -0500

    Add day01p2.py, which adds pseudodigits

--- day01/day01p2.py
+++ day01/day01p2.py
@@ -1,65 +1,60 @@
 # Advent of Code 2023
-# Day 1, Part 1
+# Day 1, Part 2
 # Parsing a list of strings for digits, then summing them 
 
 from dataclasses import dataclass
-DIGITS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
 # Problem() is my class that ingests the day's input and structures it for easy computation
 
 class Problem():
     def __init__(self, filename):
-        strings = [] # We don't need strings in this chapter but it's a neat feature
-        integers = []
+        strings = [] 
         lines = open(filename).read().splitlines()
         for line in lines:
             strings.append(line)
-            try:
-                integers.append(int(line))
-            except(ValueError):
-                integers.append(int(0))
         self.strings = strings
-        self.integers = integers
         return
 
 def digitin(string):
     for element in string:
         if element.isdigit():
-            print("Found a digit: {}".format(element))
+            # print("Found a digit: {}".format(element))
             return int(element)
 
 def distill(string):
-    newstring = string.replace('one','1')
-    newstring = newstring.replace('two','2')
-    newstring = newstring.replace('three','3')
+    newstring = string.replace('one','o1e')
+    newstring = newstring.replace('two','t2o')
+    newstring = newstring.replace('three','t3e')
     newstring = newstring.replace('four','4')
-    newstring = newstring.replace('five','5')
-    newstring = newstring.replace('six','6')
-    newstring = newstring.replace('seven','7')
-    newstring = newstring.replace('eight','8')
-    newstring = newstring.replace('nine','9')
+    newstring = newstring.replace('five','5ve')
+    newstring = newstring.replace('six','s6x')
+    newstring = newstring.replace('seven','s7en')
+    newstring = newstring.replace('eight','ei8t')
+    newstring = newstring.replace('nine','ni9e')
     return newstring
 
-# Find the total calories carried by all the elves 
+# Find the sum of 10x the first digit in the string, and 1x the last digit in the string
+# including pseudodigits 
 def codesum(problem):
     codesum = 0
     for scribble in problem.strings:
+        scribble = distill(scribble)
         tens = digitin(scribble)
         ones = digitin(reversed(scribble))
         result = 10 * tens + ones
-        print("Got a result of {}".format(result))
+        # print("Got a result of {}".format(result))
         codesum += result 
     return codesum
 
 def main():
     # Ingest and format the data
-    t = Problem("C:\\Users\\Jurph\\Documents\\Python Scripts\\aoc2023\\day01\\test.txt")
+    t = Problem("C:\\Users\\Jurph\\Documents\\Python Scripts\\aoc2023\\day01\\test2.txt")
     p = Problem("C:\\Users\\Jurph\\Documents\\Python Scripts\\aoc2023\\day01\\input.txt")
 
-    # Set up state variables 
+    # Set up state variables and run
     test = codesum(t)
     print("Sum of {} on test case.".format(test))
-    if test == 142:
+    if test == 281:
         print("...tests passed!")
         prod = codesum(p)
         print("Sum of {} on real data.".format(prod))
