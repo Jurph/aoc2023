98cdd713f45ce6c686ed79e9ebc717d9f602e412
Jurph <j.r.parsons@gmail.com>
2023-12-03 14:28:13 -0500

    Part search is working

--- day03/day03p1.py
+++ day03/day03p1.py
@@ -38,11 +38,23 @@ class GridSquare(object):
 class PartNumber:
     squares: list
 
+    def x(self) -> int:
+        return self.squares[0].x
+
+    def y(self) -> int:
+        return self.squares[0].y
+
     def value(self) -> int:
         v = ''
         for s in self.squares:
             v += s.value
         return int(v)
+
+    def neighbors(self) -> list:
+        n = []
+        for s in self.squares:
+            n.append(s.findNeighbors())
+        return n
 
 
 @dataclass(frozen=True)
@@ -50,37 +62,64 @@ class Schematic(object):
     grid: list
     width: int
     height: int
-    rowstrings: list
 
     def print(self):
         print("==========")
-        for r in self.rowstrings:
-            print(r)
-        print("==========")
-        rowstring = ''
-        for g in self.grid:
-            rowstring += str(g.value)
-            if ((g.x)+1 % int(self.width)) == self.width:
-                 print("{}".format(rowstring))
-                 rowstring = ''
+        for subgrid in self.grid:
+            rowstring = ''
+            for g in subgrid:
+                rowstring += str(g.value)
+            print("{}".format(rowstring))
         print("==========")
         print("Grid of {} x {}".format(self.width, self.height))
+    
+    def parts(self) -> list:
+        parts = []
+        for row in self.grid:
+            part = []
+            for square in row:
+                if square.value in '0123456789':
+                    part.append(square)
+                    print("Found a digit: {} - appending to part".format(square.value)) 
+                elif len(part) == 0:
+                    pass
+                else:
+                    parts.append(PartNumber(part))
+                    print("Closed out a part: {}".format(parts[-1].value()))
+                    part = []
+        return parts
 
 def MakeSchematic(filename: str) -> Schematic:
     squares = []
-    rowstrings = []
     width = 0
     height = 0
-    for (row, line) in enumerate(open(filename).read().splitlines()):
-        for (column, text) in enumerate(line):
+    # for (row, line) in enumerate(open(filename).read().splitlines()):
+        # for (column, text) in enumerate(line):
+    for (row, line) in enumerate(open(filename)):
+        subgrid = []
+        for (column, text) in enumerate(line.strip('\n')):
             if str(text) not in '1234567890.':
-                text = 'P'
-            squares.append(GridSquare(x=column, y=row, value=text))
-            # TODO: may need to put safety features on that string in case the value is '\'
+                text = 'X' # Indicates a connection point. Later we'll search for 'X' in PartNumber.neighbors 
+            subgrid.append(GridSquare(x=column, y=row, value=text))
             width = max(width, column)
             height = max(height, row)
-        rowstrings.append(line)
-    return Schematic(grid=squares, width=width+1, height=height+1, rowstrings=rowstrings) # n.b. we are zero indexed
+        squares.append(subgrid)
+    return Schematic(grid=squares, width=width+1, height=height+1) # n.b. we are zero indexed
+
+# def MakePartNumbers(s: Schematic) -> list:
+#     parts = []
+#     for row in s.grid:
+#         print("New row:\n")
+#         for square in row:
+#             squares = []
+#            if square.value in '0123456789':
+#                squares.append(square)
+#                print("Found a digit; adding {} to this part\n".format(square.value))
+#            else:
+#                print("Found a non-digit")
+#                parts.append(PartNumber(squares))
+#                squares = []
+#    return parts
 
 
 def main():
@@ -90,8 +129,14 @@ def main():
 
     print("TEST GRID:")
     t.print()
-    print("REAL GRID:")
-    p.print()
+    print("\n")
+    parts = t.parts()
+    print("We've got {} parts:".format(len(parts)))
+    for part in parts:
+        print("Found a part: {}".format(part.value()))
+
+    # print("REAL GRID:")
+    # p.print()
 
     # Run the solvers against each problem 
     # print("TEST CASE: valid games = {}".format(doTheThing(t)))
